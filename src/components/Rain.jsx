94d2188472bfd6b03632c4763835895f8f049dde
Chirag <chirag7049959351@gmail.com>
2023-07-25 13:48:18 +0530

    Update Files

---
+++ src/components/Rain.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../Rain.css";
+import "../Rain";
+import Hero from "./Hero";
+
+function Rain() {
+  return (
+    <>
+      {/* // <div className=".s-page-1 .s-section-1 .s-section"> </div> */}
+      <canvas id="sakura"></canvas>
+      <div className="btnbg"></div>
+    </>
+  );
+}
+
+export default Rain;
